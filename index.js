2185ff54ec44d1af749b9674a03b0ff0c56a8f70
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-11-18 15:27:41 +0300

    created wrapGift function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+for (let age = 30; age<40; age++){
+    console.log(`I'm ${age} years old.happy birthday to  me!`);
+
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log (`wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+    return gifts;
+}
+
+
+      
